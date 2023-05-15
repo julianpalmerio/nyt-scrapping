cf4fd64bd41b00c9f88d770ac1cd7f0522be21e7
julianpalmerio <julianpalmerio@hotmail.com>
2023-05-15 19:19:45 -0300

    feat: add configuration files

--- task.py
+++ task.py
@@ -1,8 +1,21 @@
 import logging
+
+from RPA.Robocorp.WorkItems import WorkItems
+
+
 def main():
     logger = logging.getLogger("Main Robot")
     logger.setLevel(logging.INFO)
 
+    try:
+        configuration = WorkItems().get_input_work_item().payload
+        URL = configuration["url"]
+        PHRASE = configuration["phrase"]
+    except Exception:
+        logger.exception(
+            "Error getting the configuration from Robocorp Input Work Item."
+        )
+        return
 
 def minimal_task():
     print("Done.")
